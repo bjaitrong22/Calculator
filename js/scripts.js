41258c988d400cde58f27906eeaf1c535023cd9f
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-17 23:19:44 -0600

    add subtraction, muliplication, and divsion function with separate alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1,number2) {
+  return number1/number2;
+}
+
 //user interface logic
 const number1 = parseInt(prompt("Enter a numer: "));
 const number2 = parseInt(prompt("Enter another number: "));
 
-window.alert(add(number1,number2));
+window.alert("\nThe sum is: " + add(number1,number2) + "\n" + "The difference is: " + subtract(number1, number2) + "\n" + "The product is: " + multiply(number1,number2) + "\n" + "The quotient is: " + divide(number1,number2) + "\n");
